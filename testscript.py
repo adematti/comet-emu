b23a1808117e1de0cd3f2b7361a2901f963ddcd2
Benjamín Camacho <bcamacho@Taxkari-MBP.home>
2022-04-07 18:24:30 +0200

    Add files and new directories for packaging.

--- testscript.py
+++ testscript.py
@@ -14,7 +14,7 @@ nbar = nbarsim[zsnap]
 #we create an instantiation of the emulator with the name of the
 #model we want to use
 
-Pell=PTEmu(model="ZS_EFT", use_Mpc=False)
+Pell=PTEmu(model="EFT", use_Mpc=False)
 #Pell.define_units(use_Mpc=False)
 Pell.define_nbar(nbar=nbar)
 
